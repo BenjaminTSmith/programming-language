86f5dc157bb0ef270813a2f284a1d0a7243d8802
Ben Smith <smithbenjamin@ufl.edu>
2025-11-12 18:54:10 -0500

    complete tokenize

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define true 1 
+
 typedef enum {
     NUMBER,
     OPEN_PAREN,
@@ -15,5 +17,26 @@ typedef struct {
     float number;
 } Token;
 
-int main() {
+typedef struct {
+    int start;
+    int current;
+} Tokenizer;
+
+Token tokens[2048];
+
+int tokenize(Tokenizer *tokenizer, char *str) {
+    
+}
+
+int main(int argc, char **argv) {
+    Tokenizer tokenizer;
+
+    char buffer[128];
+
+    while (true) {
+        fgets(buffer, 128, stdin);              
+
+
+        
+    }
 }
